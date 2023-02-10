7cf982b607e4f69c47a8ddad2263b8435c4e7f7b
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-10 12:52:03 +0300

    completednow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
+    const formTag = document.querySelector("form");
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    formTag.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      console.log(input.value);
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
